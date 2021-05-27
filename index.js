0a49a3e82a8e5171147fa4f9a043d3937fa81580
charulcodes <63246596+charulcodes@users.noreply.github.com>
2021-05-27 23:41:37 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdowns = document.querySelectorAll('.has-dropdown');
+
+    dropdowns.forEach((dd) => {
+        dd.addEventListener('click', function(event) {
+            event.stopPropagation();                          
+            dd.classList.toggle('is-active');
+            const content = dd.querySelector('.navbar-dropdown');
+            content.classList.toggle("is-hidden-mobile");
+         });
+    });
+
+ });
